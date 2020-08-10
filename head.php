1d84979fa0ca168cfa1da62bfe2484ff5345d563
Josiah Ogburn <21106956+logburn@users.noreply.github.com>
2020-08-09 20:27:46 -0400

    Delete head.php

--- head.php
+++
@@ -1,8 +0,0 @@
-<head>
-  <meta charset=utf-8>
-  <title>Luke Ogburn</title>
-  <link rel='shortcut icon' type=image/png href=/walker.png>
-  <link rel=stylesheet href=style-<?=(isset($_GET["theme"])&&$_GET["theme"]=="dark")?"dark":"light";?>.css>
-  <link rel=stylesheet href=style.css>
-  <meta name=viewport content="width=device-width,initial-scale=1.0">
-</head>
