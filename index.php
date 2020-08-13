546e388444a67a48910297e54129748060050bb0
Josiah Ogburn <21106956+logburn@users.noreply.github.com>
2020-08-12 20:29:27 -0400

    via web since my server lacks git

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <!--
-  Yes, I code everything myself. 
-  I know HTML, CSS, PHP, and javascript 
+  Yes, I code everything myself.
+  I know HTML, CSS, PHP, and javascript
   I avoid js wherever I can, usually substituting with CSS, but I can work with it.
 -->
 
@@ -8,29 +8,30 @@
 <html lang=en>
 <?php include "head.php"; ?>
 <body>
-    
+
 <?php include "top.php"; ?>
 
   <div id=content>
-    <p>My name is Luke Ogburn, and I'm a full stack web developer. I'm going to 
-       JMU to major in Computer Science, and I'm going to study abroad if I can. 
+    <p>My name is Luke Ogburn, and I'm a full stack web developer. I'm going to
+       JMU to major in Computer Science, and I'm going to study abroad if I can.
        I'm still mostly self taught, but I'm certainly learning a lot through JMU.<br>
-        I have recently started to use <a target=_blank href=https://github.com/logburn>github</a>, 
+        I have recently started to use <a target=_blank href=https://github.com/logburn>github</a>,
         which you should definitely check out.
     </p>
-    <p>The Chinese Communist Party has <a href=https://transitjam.com/2020/06/30/national-security-law-english-translation/#_Toc44451716 target=_blank>made it illegal world-wide</a> to criticize their rule or promote
-       freedom to Hong Kong. For that reason, I find it necessary to use this medium to express the incredible amount of disdain I hold 
+    <p>The Chinese Communist Party has <a href=https://transitjam.com/2020/06/30/national-security-law-e>
+       freedom to Hong Kong. For that reason, I find it necessary to use this medium to express the incr>
        for the CCP and declare that I fully support any efforts to end their rule in China, including
-       the restoration of the rightful autonomy of Hong Kong. <a href=https://www.reddit.com/r/HongKong/comments/czhs4q/the_five_demands_of_the_protest/ target=_blank>Five demands, not one less</a>.</p>
+       the restoration of the rightful autonomy of Hong Kong. <a href=https://www.reddit.com/r/HongKong/>
+       <a href=https://hongkongfp.com/>Hong Kong Free Press</a>, <a href=https://hk.appledaily.com/engne>
   </div>
-  
+
   <h2 class=name>CONNECT</h2>
   <div class=network>
       <a class=networkLink target=_blank href=mailto:lukeogburn@gmail.com>GMAIL</a>
       <a class=networkLink target=_blank href=mailto:lukeogburn@protonmail.com>PROTON</a>
       <a class=networkLink target=_blank href=https://github.com/logburn>GITHUB</a>
       <a class=networkLink target=_blank href=https://gitlab.com/logburn>GITLAB</a>
-      <a class=networkLink target=_blank href=https://stackoverflow.com/users/13157956/luke-ogburn>STACKOVERFLOW</a>
+      <a class=networkLink target=_blank href=https://stackoverflow.com/users/13157956/luke-ogburn>STACK>
       <a class=networkLink target=_blank href=https://linkedin.com/in/logburn>LINKEDIN</a>
       <a class=networkLink target=_blank href=https://pixelfed.social/logburn>PIXELFED</a>
       <a class=networkLink target=_blank href=resume/>RESUME</a>
