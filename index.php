9033afb8f708ce7c75d2fa926fe8529057fdfd19
Josiah Ogburn <21106956+logburn@users.noreply.github.com>
2020-08-17 23:33:03 -0400

    Update index.php

--- index.php
+++ index.php
@@ -12,17 +12,17 @@
 <?php include "top.php"; ?>
 
   <div id=content>
-    <p>My name is Luke Ogburn, and I'm a full stack web developer. I'm going to
+    <p>My name is <span class="letter">L</span><span class="letter">u</span><span class="letter">k</span><span class="letter">e</span> <span class="letter">O</span><span class="letter">g</span><span class="letter">b</span><span class="letter">u</span><span class="letter">r</span><span class="letter">n</span>, and I'm a full stack web developer. I'm going to
        JMU to major in Computer Science, and I'm going to study abroad if I can.
        I'm still mostly self taught, but I'm certainly learning a lot through JMU.<br>
         I have recently started to use <a target=_blank href=https://github.com/logburn>github</a>,
         which you should definitely check out.
     </p>
-    <p>The Chinese Communist Party has <a href=https://transitjam.com/2020/06/30/national-security-law-e>
-       freedom to Hong Kong. For that reason, I find it necessary to use this medium to express the incr>
+    <p>The Chinese Communist Party has <a href=https://transitjam.com/2020/06/30/national-security-law-english-translation/#_Toc44451716 target=_blank>made it illegal world-wide</a> to criticize their rule or promote
+       freedom to Hong Kong. For that reason, I find it necessary to use this medium to express the incredible amount of disdain I hold 
        for the CCP and declare that I fully support any efforts to end their rule in China, including
-       the restoration of the rightful autonomy of Hong Kong. <a href=https://www.reddit.com/r/HongKong/>
-       <a href=https://hongkongfp.com/>Hong Kong Free Press</a>, <a href=https://hk.appledaily.com/engne>
+       the restoration of the rightful autonomy of Hong Kong. <a href=https://www.reddit.com/r/HongKong/comments/czhs4q/the_five_demands_of_the_protest/ target=_blank>Five demands, not one less</a>.<br>
+       <a href=https://hongkongfp.com/>Hong Kong Free Press</a>, <a href=https://hk.appledaily.com/engnews/>Apple Daily</a>.</p>
   </div>
 
   <h2 class=name>CONNECT</h2>
@@ -31,7 +31,7 @@
       <a class=networkLink target=_blank href=mailto:lukeogburn@protonmail.com>PROTON</a>
       <a class=networkLink target=_blank href=https://github.com/logburn>GITHUB</a>
       <a class=networkLink target=_blank href=https://gitlab.com/logburn>GITLAB</a>
-      <a class=networkLink target=_blank href=https://stackoverflow.com/users/13157956/luke-ogburn>STACK>
+      <a class=networkLink target=_blank href=https://stackoverflow.com/users/13157956/luke-ogburn>STACKOVERFLOW</a>
       <a class=networkLink target=_blank href=https://linkedin.com/in/logburn>LINKEDIN</a>
       <a class=networkLink target=_blank href=https://pixelfed.social/logburn>PIXELFED</a>
       <a class=networkLink target=_blank href=resume/>RESUME</a>
