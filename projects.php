b5f52d3d240e7c6ea5258e336bb121056d997e5b
luke <21106956+logburn@users.noreply.github.com>
2020-10-22 23:34:24 -0400

    readded links

--- projects.php
+++ projects.php
@@ -32,7 +32,7 @@ This site was based off of this site. I basically wanted to only write 500 words
 <h2>Blog:</h2>
 This blog never really amounted to much as I never really maintained it. It was mostly thrown together because I wanted to practice my PHP, so even though the blog itself is basically a failure I still gained a lot from it in terms of experience. I plan to bring it back one day, but I currently have no real reason to do so.
     <br>Technologies used: HTML, CSS, PHP, PDO, SQL<br>
-    This site is now down; I do not blog anymore (might be up in the future).
+    You can find it <a href=https://blog.lukeogburn.com/>here</a>, though I don't post much (at all...).
 
 <h2>Rubik's Cube:</h2>
 This was actually the first page I made. It teaches methods on solving various Rubik's Cubes and mostly taught me CSS techniques. The original design was done with Wix before I knew anything about web development. It got me going in this whole business.<br>
@@ -42,7 +42,7 @@ This was actually the first page I made. It teaches methods on solving various R
 <h2>IB Site:</h2>
 This is basically a social media site. I designed it to be used for school-based things such as uploading notes or asking questions. It can, theoretically, be used for anything, but it is really designed for uploading notes that have been saved as an image file or as a document file. This site taught me a ton about the technologies I used and the interactions between them. Note that you currently need a school-assigned HCPS email account to access the website (this will be changing at some point, but has not yet). Unfortunately, this site is now broken due to my school changing the email addresses for students. It requires an email to login, so while students can log in, there are a few things that don't work (like tagging users). It isn't worth the effort to maintain, so I'm not going to edit it until I spontaneously get the energy.
     <br>Technologies used: HTML, CSS, JS, PHP, PDO, SQL, Google OAuth API, Regex<br>
-    This site is currently not working, since it requries a Google Suite account, the domain of which has shut down recently. I plan to get it up (non-functionally) at some point.
+    You can find it <a href=https://ib.lukeogburn.com/>here</a>, but functionality may be limited and you will be on a "guest" account.
 
 <h2>URL Shortener:</h2>
 This site was fun to make and I do use it myself. All it does is take a URL and make it into another url. For example, lukeogburn.com is shortened to lukeo.link/TvVif. If a URL is submitted again, it will use the same short URL as before. I did buy a separate domain for this site, so I won't be keeping up with it once I decide that it's not worth the price. No idea when that'll be, so for now it's only use is as a temporary URL shortener for simple convenience.
