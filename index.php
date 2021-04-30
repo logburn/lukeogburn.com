d4f5268910c04647e825cc0ea23c9bd351141934
luke <21106956+logburn@users.noreply.github.com>
2021-04-30 01:35:25 -0400

    removed all promo comments and corrected github name

--- index.php
+++ index.php
@@ -1,9 +1,3 @@
-<!--
-  Yes, I code everything myself.
-  I know HTML, CSS, PHP, and javascript
-  I avoid js wherever I can, usually substituting with CSS, but I can work with it.
--->
-
 <!doctype html>
 <html lang=en>
 <?php include "head.php"; ?>
@@ -12,7 +6,7 @@
 <?php include "top.php"; ?>
 
   <div id=content>
-    <p>My name is <span class="letter">L</span><span class="letter">u</span><span class="letter">k</span><span class="letter">e</span> <span class="letter">O</span><span class="letter">g</span><span class="letter">b</span><span class="letter">u</span><span class="letter">r</span><span class="letter">n</span>, and I'm a software developer studying Computer Science at James Madison University. I'm interested in technologies that give power to the common people, such as encryption and federation. I'm always on the lookout for new ideas or projects that fit this category. On that note, I am a strong supporter of open source. I also practice what I preach: almost all of my code is available on my <a target=_blank href=https://github.com/logburn>Github</a> and is licensed under the <a target=_blank href=https://www.gnu.org/licenses/gpl-3.0.en.html>GNU GPLv3</a>. On the flip side, I also despise those who seek to control others, such as the Chinese Communist Party, or even the United State's own far-overreaching practices.
+    <p>My name is <span class="letter">L</span><span class="letter">u</span><span class="letter">k</span><span class="letter">e</span> <span class="letter">O</span><span class="letter">g</span><span class="letter">b</span><span class="letter">u</span><span class="letter">r</span><span class="letter">n</span>, and I'm a software developer studying Computer Science at James Madison University. I'm interested in technologies that give power to the common people, such as encryption and federation. I'm always on the lookout for new ideas or projects that fit this category. On that note, I am a strong supporter of open source. I also practice what I preach: almost all of my code is available on my <a target=_blank href=https://github.com/logburn>GitHub</a> and is licensed under the <a target=_blank href=https://www.gnu.org/licenses/gpl-3.0.en.html>GNU GPLv3</a>. On the flip side, I also despise those who seek to control others, such as the Chinese Communist Party, or even the United State's own far-overreaching practices.
     </p>
 </div>
 
