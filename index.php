0b4f9b5dc61ca4368f804be84e4ccd1cb4db37c5
Luke Ogburn <21106956+logburn@users.noreply.github.com>
2022-01-24 15:13:50 +0000

    removed message
    to appease my mother :p

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 <?php include "top.php"; ?>
 
   <div id=content>
-    <p>My name is <span class="letter">L</span><span class="letter">u</span><span class="letter">k</span><span class="letter">e</span> <span class="letter">O</span><span class="letter">g</span><span class="letter">b</span><span class="letter">u</span><span class="letter">r</span><span class="letter">n</span>, and I'm a software developer studying Computer Science at James Madison University. I'm interested in technologies that give power to the common people, such as encryption and federation. I'm always on the lookout for new ideas or projects that fit this category. On that note, I am a strong supporter of open source. I also practice what I preach: almost all of my code is available on my <a target=_blank href=https://github.com/logburn>GitHub</a> and is licensed under the <a target=_blank href=https://www.gnu.org/licenses/gpl-3.0.en.html>GNU GPLv3</a>. On the flip side, I also despise those who seek to control others, such as the Chinese Communist Party, or even the United State's own far-overreaching practices.
+    <p>My name is <span class="letter">L</span><span class="letter">u</span><span class="letter">k</span><span class="letter">e</span> <span class="letter">O</span><span class="letter">g</span><span class="letter">b</span><span class="letter">u</span><span class="letter">r</span><span class="letter">n</span>, and I'm a software developer studying Computer Science at James Madison University. I'm interested in technologies that give power to the common people, such as encryption and federation. I'm always on the lookout for new ideas or projects that fit this category. On that note, I am a strong supporter of open source. I also practice what I preach: almost all of my code is available on my <a target=_blank href=https://github.com/logburn>GitHub</a> and is licensed under the <a target=_blank href=https://www.gnu.org/licenses/gpl-3.0.en.html>GNU GPLv3</a>.
     </p>
 </div>
 
@@ -27,3 +27,20 @@
   <a rel="me" href="https://mastodon.online/@logburn" style=display:none>Mastodon</a>
 </div></body>
 </html>
+
+<!--
+-----BEGIN PGP MESSAGE-----
+
+hF4DUXnhiRMRr6ESAQdA2Ft56V7sp00EyurNysKetfMrrjEWsZ8HvhUBBBq6SWww
+jD3m4UTNqVzELBqkhoDluGCRED/4NTYYJxOYHq8AyuGR9Zkg8+Cb7uJkU43ixAjr
+0sCYAXV5Kr3vUTydhcAX67YilNCbdaYvcdCxPIYQ/Yre8sEEIQXReJCy4kr0aaGc
+EsXQRvnur+g1KKUkMTeuLSf2KAn5Nuy3+XarBod8JkPgWuBGK2A6gj3X92KzJfyz
+TzBLX2keh8B+dAsI1IeawIb0d/obD8pESYXdqvOd9SU81RDyCr9bDr+WGLmHHFZ/
+JwRCNBunUSrZDdPIuHQAyDix5SxbmpjWcaoReGzR2xzj5sRj31y2u3efu9TiZUQ/
+8oRU2C3ojF5q+NyrKKe7MEFhcfKkEubzQL9yM6NJoXfRZ+fl8szGjqtgH4gMj85y
+WVwWnWMZ9IDQhxvcxMGZG27uonG6eFcFN9XbfNdlN1Q8HJTPg4nn1ZRfBUCfWY04
+KANTXdX7G+osVjiEcJIPdE1rEThkMSX+ku9IfADS1CcCM7nLzI8XMdhOaXj2YpMc
+4jbGfgik0wwkB94=
+=1AQ3
+-----END PGP MESSAGE-----
+-->
