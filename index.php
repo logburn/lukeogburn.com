58882900b8715ad477421f1ebdd10c8da3870769
Luke Ogburn <21106956+logburn@users.noreply.github.com>
2024-03-22 16:32:41 -0400

    Update intro paragraph

--- index.php
+++ index.php
@@ -6,8 +6,7 @@
 <?php include "top.php"; ?>
 
   <div id=content>
-    <p>My name is <span class="letter">L</span><span class="letter">u</span><span class="letter">k</span><span class="letter">e</span> <span class="letter">O</span><span class="letter">g</span><span class="letter">b</span><span class="letter">u</span><span class="letter">r</span><span class="letter">n</span>, and I'm a software developer studying Computer Science at James Madison University. I'm interested in technologies that give power to the common people, such as encryption and federation. I'm always on the lookout for new ideas or projects that fit this category. On that note, I am a strong supporter of open source. I also practice what I preach: almost all of my code is available on my <a target=_blank href=https://github.com/logburn>GitHub</a> and is licensed under the <a target=_blank href=https://www.gnu.org/licenses/gpl-3.0.en.html>GNU GPLv3</a>.
-    </p>
+    <p>My name is <span class="letter">L</span><span class="letter">u</span><span class="letter">k</span><span class="letter">e</span> <span class="letter">O</span><span class="letter">g</span><span class="letter">b</span><span class="letter">u</span><span class="letter">r</span><span class="letter">n</span>, and I'm a software developer currently working as a full-stack web developer at <a target="_blank" href="https://www.consultwithcase.com/">CASE</a>. I'm currently getting my Master's degree in data science at <a target="_blank" href="https://www.eastern.edu/">Eastern University</a>, potentially looking to specialize in NLP. I really enjoy helping people and solving problems. I also enjoy reading, hiking, and studying Christian theology. I love open-source, and most of the code I write is available on <a target="_blank" href="https://github.com/logburn">GitHub</a> and is licensed under the <a target="_blank" href="https://www.gnu.org/licenses/gpl-3.0.en.html">GNU GPLv3</a>.</p>
 </div>
 
   <h2 class=name>CONNECT</h2>
